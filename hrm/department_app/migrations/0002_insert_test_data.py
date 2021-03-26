42ba1ac18d42d124e80f854defca721591767dd4
Master <krisstinkou@gmail.com>
2021-03-26 20:47:21 +0300

    Fixes in step 10 (DB).

--- hrm/department_app/migrations/0002_insert_test_data.py
+++ hrm/department_app/migrations/0002_insert_test_data.py
@@ -3,20 +3,42 @@
 from django.db import migrations
 
 
-def insert_data(apps, schema_editor):
+def forward_insert_data(apps, schema_editor):
     Department = apps.get_model('department_app', 'Department')
-    dep_dev = Department(name='Development')
-    dep_dev.save()
-    dep_test = Department(name='Testing')
-    dep_test.save()
-    dep_fin = Department(name='Finance')
-    dep_fin.save()
-    dep_dev.employee_set.create(name='Renee Stokes', date_of_birth='1997-04-26', salary=1500.0)
-    dep_dev.employee_set.create(name='Abbi Finch', date_of_birth='1993-09-04', salary=3500.0)
-    dep_fin.employee_set.create(name='Ahmet Hayes', date_of_birth='1994-02-19', salary=1100.0)
-    dep_fin.employee_set.create(name='Kaylegh Pineda', date_of_birth='1999-07-29', salary=800.0)
-    dep_test.employee_set.create(name='Almie Patel', date_of_birth='1995-11-12', salary=1000.0)
-    dep_test.employee_set.create(name='Neo Tumbull', date_of_birth='1995-11-12', salary=1000.0)
+    Employee = apps.get_model('department_app', 'Employee')
+
+    Department.objects.bulk_create([
+        Department(name='Development'),
+        Department(name='Testing'),
+        Department(name='Finance'),
+    ])
+
+    dep_dev = Department.objects.get(name='Development')
+
+    dep_dev.employee_set.bulk_create([
+        Employee(name='Renee Stokes', date_of_birth='1997-04-26', salary=1500.0, related_department=dep_dev),
+        Employee(name='Abbi Finch', date_of_birth='1993-09-04', salary=3500.0, related_department=dep_dev),
+    ])
+
+    dep_fin = Department.objects.get(name='Finance')
+    dep_fin.employee_set.bulk_create([
+        Employee(name='Ahmet Hayes', date_of_birth='1994-02-19', salary=1100.0, related_department=dep_fin),
+        Employee(name='Kaylegh Pineda', date_of_birth='1999-07-29', salary=800.0, related_department=dep_fin),
+    ])
+
+    dep_test = Department.objects.get(name='Testing')
+    dep_test.employee_set.bulk_create([
+        Employee(name='Almie Patel', date_of_birth='1995-11-12', salary=1000.0, related_department=dep_test),
+        Employee(name='Neo Tumbull', date_of_birth='1995-11-12', salary=1000.0, related_department=dep_test),
+    ])
+
+
+def backward_delete_data(apps, schema_editor):
+    Department = apps.get_model('department_app', 'Department')
+    Employee = apps.get_model('department_app', 'Employee')
+
+    Employee.objects.all().delete()
+    Department.objects.all().delete()
 
 
 class Migration(migrations.Migration):
@@ -28,5 +50,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(insert_data),
+        migrations.RunPython(forward_insert_data, backward_delete_data),
     ]
--- hrm/department_app/models/__init__.py
+++ hrm/department_app/models/__init__.py
@@ -2,5 +2,5 @@
 Module in which models are searched.
 """
 
-from .department import *
-from .employee import *
+from .department import Department
+from .employee import Employee
