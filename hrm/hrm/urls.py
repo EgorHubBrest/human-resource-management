43da5bcd7decb73a810dd511ef1112a66921d77c
Master <krisstinkou@gmail.com>
2021-04-08 18:55:04 +0300

    Removed updating debug.log file on github. Removed excess line in urls.py

--- hrm/hrm/urls.py
+++ hrm/hrm/urls.py
@@ -28,5 +28,4 @@ router.register(r'employee', EmployeeViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
