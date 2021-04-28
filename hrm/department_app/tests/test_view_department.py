778aabca6fe07255339d5ba3ca409271a9ac2f07
Master <krisstinkou@gmail.com>
2021-04-28 15:55:04 +0300

    Test urls for tests (addition)

--- hrm/department_app/tests/test_view_department.py
+++ hrm/department_app/tests/test_view_department.py
@@ -14,6 +14,6 @@ class TestViewDepartment(TestCase):
 
     def test_create_department(self):
         dep = Department.objects.create(name='NewTestDepartment')
-        response = self.client.get('/department/')
+        response = self.client.get(reverse('department-list', format='json'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, dep.name)
