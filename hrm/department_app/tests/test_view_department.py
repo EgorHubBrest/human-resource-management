6465980d824c6f38fe2ce26a017f32b508997dae
Master <krisstinkou@gmail.com>
2021-04-30 17:24:59 +0300

    Rewrited tests (with changed serializer for Employee)

--- hrm/department_app/tests/test_view_department.py
+++ hrm/department_app/tests/test_view_department.py
@@ -1,19 +1,68 @@
 from django.test import TestCase, Client
+from django.utils.http import urlencode
 from ..models.department import Department
 from rest_framework.reverse import reverse
+from rest_framework import status
+from django.forms.models import model_to_dict
 
 
 class TestViewDepartment(TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.department_data = {
+            "name": "Test Department",
+        }
+        self.expected_keys = {key.name for key in Department._meta.fields}
+        self.department = Department.objects.create(name="Old Test Department")
 
-    def test_get_department(self):
-        response = self.client.get(reverse('department-list', format='json'))
-        self.assertEqual(response.status_code, 200)
+    def test_department_create(self):
+        """
+        Test POST request for REST API
+        """
 
-    def test_create_department(self):
-        dep = Department.objects.create(name='NewTestDepartment')
-        response = self.client.get(reverse('department-list', format='json'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, dep.name)
+        department_count = Department.objects.count()
+        response = self.client.post(reverse('department-list'), data=self.department_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Department.objects.count(), department_count + 1)
+        self.assertEqual(response.data.keys(), self.expected_keys)
+        self.assertEqual(response.data, model_to_dict(Department.objects.get(name=self.department_data['name']),
+                                                                            fields=self.expected_keys))
+
+    def test_department_read(self):
+        """
+        Test GET request for REST API
+        """
+
+        department_count = Department.objects.count()
+        response = self.client.get(reverse('department-list'), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), department_count)
+        if department_count > 0:
+            self.assertEqual(response.data[0].keys(), self.expected_keys)
+
+    def test_department_update(self):
+        """
+        Test UPDATE request for REST API
+        """
+
+        department_count = Department.objects.count()
+        response = self.client.put(reverse('department-detail', args=(self.department.id,)),
+                                   data=urlencode(self.department_data),
+                                   content_type='application/x-www-form-urlencoded')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Department.objects.count(), department_count)
+        self.assertEqual(response.data.keys(), self.expected_keys)
+        self.assertEqual(response.data, model_to_dict(Department.objects.get(id=self.department.id),
+                                                      fields=self.expected_keys))
+        self.assertEqual(Department.objects.get(id=self.department.id).name, self.department_data["name"])
+
+    def test_department_delete(self):
+        """
+        Test DELETE request for REST API
+        """
+
+        department_count = Department.objects.count()
+        response = self.client.delete(reverse('department-detail', args=(self.department.id,)))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Department.objects.count(), department_count - 1)
--- hrm/department_app/tests/test_view_employee.py
+++ hrm/department_app/tests/test_view_employee.py
@@ -1,12 +1,84 @@
 from django.test import TestCase, Client
 from ..models.employee import Employee
 from ..models.department import Department
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
+import datetime
+from rest_framework.reverse import reverse
+from rest_framework import status
+from django.utils.http import urlencode
+from django.forms.models import model_to_dict
 
 
 class TestViewEmployee(TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.expected_keys = {key.name for key in Employee._meta.fields}
+        self.department = Department.objects.create(name="Test Department")
+        self.employee = Employee.objects.create(name="Old Test Employee", salary=1000.0,
+                                                date_of_birth=datetime.datetime(year=2000, month=9, day=9).strftime("%Y-%m-%d"),
+                                                related_department=self.department)
+        self.employee_data = {
+            "name": "Test Employee",
+            "date_of_birth": datetime.datetime(year=1999, month=9, day=9).strftime("%Y-%m-%d"),
+            "salary": 1000.0,
+            "related_department": self.department.id
+        }
 
+    def test_employee_create(self):
+        """
+        Test POST request for REST API
+        """
+
+        employee_count = Employee.objects.count()
+        response = self.client.post(reverse('employee-list'), data=self.employee_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Employee.objects.count(), employee_count + 1)
+        self.assertEqual(response.data.keys(), self.expected_keys)
+        new_emp_dict = model_to_dict(Employee.objects.get(name=self.employee_data['name']), fields=self.expected_keys)
+        for field in self.expected_keys:
+            if isinstance(new_emp_dict[field], datetime.date):
+                self.assertEqual(response.data[field], new_emp_dict[field].strftime("%Y-%m-%d"))
+            else:
+                self.assertEqual(response.data[field], new_emp_dict[field])
+
+    def test_department_read(self):
+        """
+        Test GET request for REST API
+        """
+
+        employee_count = Employee.objects.count()
+        response = self.client.get(reverse('employee-list'), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), employee_count)
+        if employee_count > 0:
+            self.assertEqual(response.data[0].keys(), self.expected_keys)
+
+    def test_department_update(self):
+        """
+        Test UPDATE request for REST API
+        """
+
+        employee_count = Employee.objects.count()
+        response = self.client.put(reverse('employee-detail', args=(self.employee.id,)),
+                                   data=urlencode(self.employee_data),
+                                   content_type='application/x-www-form-urlencoded')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Employee.objects.count(), employee_count)
+        self.assertEqual(response.data.keys(), self.expected_keys)
+        new_emp_dict = model_to_dict(Employee.objects.get(id=self.employee.id), fields=self.expected_keys)
+        for field in self.expected_keys:
+            if isinstance(new_emp_dict[field], datetime.date):
+                self.assertEqual(response.data[field], new_emp_dict[field].strftime("%Y-%m-%d"))
+            else:
+                self.assertEqual(response.data[field], new_emp_dict[field])
+        self.assertEqual(new_emp_dict["name"], self.employee_data["name"])
+
+    def test_department_delete(self):
+        """
+        Test DELETE request for REST API
+        """
+
+        employee_count = Employee.objects.count()
+        response = self.client.delete(reverse('employee-detail', args=(self.employee.id,)))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Employee.objects.count(), employee_count - 1)
